42af117893055d440083120a40129ac694c4831d
Paolo Cabaleiro <cabaleirog@gmail.com>
2021-08-24 16:25:15 -0400

    Completed a minimal implementation of the endpoint

--- locations-api-service/djangoapi/djangoapi/settings.py
+++ locations-api-service/djangoapi/djangoapi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'locations',
+        'USER': 'root',
+        'PASSWORD': 'demo-mysql-secret-password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
